ee3673cd5e4a8146e4c3c24ef95591eec0f149a2
GD-PeaceMinusone <Jackeylove@jackeylovedemacbook-pro.local>
2017-03-08 22:00:01 +0800

    完善登录界面

--- 百思不得姐/百思不得姐/classes/Other/Controller/XMGLoginRegisterViewController.m
+++ 百思不得姐/百思不得姐/classes/Other/Controller/XMGLoginRegisterViewController.m
@@ -9,15 +9,39 @@
 #import "XMGLoginRegisterViewController.h"
 
 @interface XMGLoginRegisterViewController ()
+//@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftMargin;
 @end
 
 @implementation XMGLoginRegisterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+//    [self.loginButton setValue:@5 forKeyPath:@"layer.cornerRadius"];
+//    [self.loginButton setValue:@YES forKeyPath:@"layer.masksToBounds"];
+//    
 }
+- (IBAction)showLoginOrRegister:(UIButton *)button {
+    [self.view endEditing:YES];
+    
+    if (self.leftMargin.constant) {
+        self.leftMargin.constant = 0;
+        [button setTitle:@"注册账号" forState:UIControlStateNormal];
+        
+    }else {
+    
+        self.leftMargin.constant = -self.view.xmg_width;
+        [button setTitle:@"已有账号?" forState:UIControlStateNormal];
+    }
+    [UIView animateWithDuration:0.3 animations:^{
+        
+        [self.view layoutIfNeeded];
+    }];
+    
+
+}
+
 
 -(UIStatusBarStyle)preferredStatusBarStyle {
 
@@ -28,5 +52,11 @@
     
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+
+    [self.view endEditing:YES];
+}
+
 
 @end
